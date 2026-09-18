2766e3c8924362ef7e0e00c2679ebe40bb480008
Alonso Molina <alonso.19.2005@gmail.com>
2026-09-18 17:40:32 -0600

    Se agregan comentarios a la entidad Student

--- src/main/java/com/webintegral/model/Student.java
+++ src/main/java/com/webintegral/model/Student.java
@@ -15,21 +15,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "student")
+// Entidad que representa a un estudiante en la base de datos
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Identificador único del estudiante
     private Integer id;
 
     @Column(length = 80)
+    // Nombre del estudiante
     private String name;
 
     @Column(name = "last_name", length = 80)
+    // Apellido del estudiante
     private String lastName;
 
     @Column(length = 20)
+    // Teléfono de contacto
     private String phone;
 
     @Column(length = 80)
+    // Correo electrónico
     private String email;
 }
